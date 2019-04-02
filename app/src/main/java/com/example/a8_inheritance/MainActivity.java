035e60744a0cf1812720d1696edb0335747868d1
karita <kari.aaltonen@metropolia.fi>
2019-04-02 12:33:10 +0300

    Person tested

--- app/src/main/java/com/example/a8_inheritance/MainActivity.java
+++ app/src/main/java/com/example/a8_inheritance/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.a8_inheritance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import people.Person;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvPersons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Test Person class
+        tvPersons = findViewById(R.id.tvPerson);
+        Person p1 = new Person("Mary Poppins");
+        Person p2 = new Person("Harry Potter");
+        tvPersons.setText("**Persons \n" +
+                p1.getInfo() +"\n" + p2.getInfo());
+
     }
 }
--- app/src/main/java/people/Person.java
+++ app/src/main/java/people/Person.java
@@ -1,6 +1,6 @@
 package people;
 
-public class Person<RecentlyNonNull> {
+public class Person {
     private String name;
     public Person(String name) {
         this.name=name;
